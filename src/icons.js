ea22e4b41db5ee9311d569d13f0023fc09109900
PKLJack <59538991+PKLJack@users.noreply.github.com>
2022-07-12 16:52:59 +0100

    remove create-react-app stuff

--- src/icons.js
+++
@@ -1,5 +0,0 @@
-import { ReactComponent as GithubIcon } from "bootstrap-icons/icons/github.svg"
-import { ReactComponent as TwitterIcon } from "bootstrap-icons/icons/twitter.svg"
-import { ReactComponent as LinkedInIcon } from "bootstrap-icons/icons/linkedin.svg"
-
-export { GithubIcon, TwitterIcon, LinkedInIcon }
--- src/images/readme.txt
+++
@@ -1,6 +0,0 @@
-Images:
-	ratio:
-		width: 3
-		height: 1
-
-	location: Public folder
