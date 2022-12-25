15af965e63d2e4aa949a21f275d3324a11d9719b
Pui Kit Li <59538991+PKLJack@users.noreply.github.com>
2022-12-25 03:36:03 +0000

    Fix formatting

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -4,28 +4,19 @@ import styles from "../styles/SocialLinks.module.scss"
 
 export default function SocialLinks() {
   return (
-    <div className={styles["social-links"]}>
-      <a
-        href="https://github.com/PKLJack"
-        className={styles["social-links__btn"]}
-      >
-        <Icon name="github" />
-        <span>Github</span>
-      </a>
-      <a
-        href="https://twitter.com/PKL_Jack"
-        className={styles["social-links__btn"]}
-      >
-        <Icon name="linkedin" />
-        <span>Twitter</span>
-      </a>
-      <a
-        href="https://www.linkedin.com/in/pui-kit-li-793555177/"
-        className={styles["social-links__btn"]}
-      >
-        <Icon name="twitter" />
-        <span>LinkedIn</span>
-      </a>
-    </div>
+    <>
+      {/* prettier-ignore */}
+      <div className={styles["social-links"]}>
+        <a href="https://github.com/PKLJack" className={styles["social-links__btn"]}>
+          <Icon name="github"/><span>Github</span> 
+        </a>
+        <a href="https://twitter.com/PKL_Jack" className={styles["social-links__btn"]}>
+          <Icon name="linkedin"/><span>Twitter</span>
+        </a>
+        <a href="https://www.linkedin.com/in/pui-kit-li-793555177/" className={styles["social-links__btn"]}>
+          <Icon name="twitter"/><span>LinkedIn</span>
+        </a>
+      </div>
+    </>
   )
 }
