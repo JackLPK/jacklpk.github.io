1615d965c45f0e11b8d8967ac46babba2483151b
Pui Kit Li <59538991+PKLJack@users.noreply.github.com>
2022-12-25 03:38:46 +0000

    Update utils.ts

--- utils.ts
+++ utils.ts
@@ -1,32 +1,5 @@
 import { CardData, TagData } from "./models"
 
-function sortTags(arr1: Array<string>): string[] {
-  const importance = ["ReactJS", "JavaScript", "Python", "R"]
-  importance.reverse()
-
-  let arr2 = Array.from(arr1)
-
-  for (const tag of importance) {
-    const itemIndex = arr2.indexOf(tag)
-    if (itemIndex !== -1) {
-      arr2.splice(itemIndex, 1)
-      arr2.unshift(tag)
-    }
-  }
-
-  return arr2
-}
-
-function reduceTags(cardsArray: any[]): string[] {
-  return Array.from(
-    new Set(
-      cardsArray.reduce((prev, curr) => {
-        return [...prev, ...curr.tags]
-      }, [])
-    )
-  )
-}
-
 /* Returns descending ordered TagData[] */
 function calculateTags(cardsArray: CardData[]): TagData[] {
   const counter: any = {}
@@ -51,4 +24,4 @@ function calculateTags(cardsArray: CardData[]): TagData[] {
   return pairs
 }
 
-export { sortTags, reduceTags, calculateTags }
+export { calculateTags }
