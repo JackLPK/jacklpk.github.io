210d83919902beda25e2cea6ec26195c09965ac1
Pui Kit Li <59538991+PKLJack@users.noreply.github.com>
2023-06-22 22:38:41 +0100

    Delete styles to move to tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: "class",
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      // fontSize: {
+      //   base: "18px",
+      // },
+      fontFamily: {
+        sans: ["var(--font-iter)", "Roboto", "Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: "hsl(320, 53%, 52%)",
+      },
+      // backgroundImage: {
+      //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      //   'gradient-conic':
+      //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // },
+    },
+  },
+  plugins: [],
+}
